380920236e8de99e7800cd3be32b8cc7bb2be94b
Debraj Rakshit <debrajrakshit@Debrajs-MacBook-Air.local>
2019-08-30 15:57:44 +0530

    test cases added

--- src/index.js
+++ src/index.js
@@ -1,16 +1,5 @@
-const bcrypt = require('bcryptjs');
-require('./db/mongoose');
-const userRouter = require('./routers/user');
-const taskRouter = require('./routers/task');
-
-const express = require('express');
-const app = express();
-
+const app = require('./app');
 const port = process.env.PORT;
-
-app.use(express.json());
-app.use(userRouter);
-app.use(taskRouter);
 
 // listen to port
 app.listen(port, () => {
