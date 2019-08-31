1bf6ba68abac2782c920d1a6c682561ac78ca5c9
Debraj Rakshit <debrajrakshit@Debrajs-MacBook-Air.local>
2019-08-31 11:33:27 +0530

    Assertion added to the tests, delete test failed with response 500

--- tests/user.test.js
+++ tests/user.test.js
@@ -21,18 +21,41 @@ beforeEach(async () => {
 });
 
 test('Should sign up a new user', async () => {
-    await request(app).post('/users').send({
+    const response = await request(app).post('/users').send({
         name: 'Andrew',
         email: 'andrew@example.com',
         password: '12345678'
     }).expect(201);
+
+    // assert that the database was changed correctly
+    const user = await User.findById(response.body.user._id);
+    expect(user).not.toBeNull();
+
+    // Assertion about the response
+    expect(response.body).toMatchObject({
+        user: {
+            name: 'Andrew',
+            email: 'andrew@example.com'
+        },
+        token: user.tokens[0].token
+    });
+
+    expect(user.password).not.toBe('12345678');
 });
 
 test('Should login exisiting user', async () => {
-    await request(app).post('/users/login').send({
+    const response = await request(app).post('/users/login').send({
         email: userOne.email,
         password: userOne.password
     }).expect(200);
+
+    const user = await User.findById(response.body.user._id);
+    expect(user).not.toBeNull();
+
+    // assert about the response
+    expect(response.body).toMatchObject({
+        token: user.tokens[1].token
+    });
 });
 
 test('Should not login nonexistent user', async () => {
@@ -58,11 +81,16 @@ test('Should not get profile for unauthenticated user', async() => {
 });
 
 test('Should delete account for user', async () => {
-   await request(app)
+   const response = await request(app)
         .delete('/users/me')
         .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
         .send()
         .expect(200);
+
+    const user = await User.findById(userOneId);
+    expect(user).toBeNull();
+
+    expect(response.body).toBeNull();
 });
 
 test('Should not delete account for unauthenticated user', async () => {
